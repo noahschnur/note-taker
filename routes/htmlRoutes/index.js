7ec685a4555fa48222e4d3cbed486c430591eaec
Noah <noahschnur@gmail.com>
2021-04-17 12:09:24 -0700

    move index.html

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -7,7 +7,7 @@ router.get('/notes', (req, res) => {
 });
 
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
+    res.sendFile(path.join(__dirname, '../../index.html'));
 });
 
 module.exports = router;
