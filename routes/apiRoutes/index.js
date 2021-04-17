30f19eab16a5fb0959472666928bc3648327593c
Noah <noahschnur@gmail.com>
2021-04-17 12:03:46 -0700

    organize and pretty code

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,47 +1,39 @@
 const router = require('express').Router();
 const fs = require('fs');
-const { validateNote, createNewNote } = require('../../lib/notes');
 const notes = require('../../db/db.json');
 const { v4: uuidv4 } = require('uuid');
 
-
+// GET notes
 router.get('/notes', (req, res) => {
     const note = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
     res.json(notes.notes);
 });
 
+// POST notes, add id via uuidv4
 router.post('/notes', (req, res) => {
     notes.notes = notes.notes.concat({...req.body, id: uuidv4()});
     // console.log(uuidv4());
+    // Rewrite db.json with new POST data
     fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
         if (err) throw err;
         res.json(notes.notes);
-        console.log('db saved');
+        // console.log('db saved');
     })
-    var tempArray = notes.notes.concat(req.body);
-    // console.log(tempArray);
-    // console.log(req.body);
-    // console.log(notes.notes.length);
-
-//     if (!validateNote(req.body)) {
-//         res.status(400).send('The note is not properly formatted');
-//     } else {
-//         const note = createNewNote(req.body, notes);
-//         res.json(note);
-//     }
 });
 
+// DELETE notes from array
 router.delete('/notes/:id', (req, res) => {
     var filteredNotesArray = notes.notes.filter(function(note){ 
             return note.id != req.params.id; 
         })
         notes.notes = filteredNotesArray
+        // Rewrite db.json without note with ID selected by DELETE
         fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
             if (err) throw err;
             res.json(notes.notes);
             console.log('db saved');
         })
-    console.log(filteredNotesArray);
+    // console.log(filteredNotesArray);
 });
 
 module.exports = router;
